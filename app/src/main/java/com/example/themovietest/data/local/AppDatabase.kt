6d09ef560bca453d6d146dd27a18ad9a296df938
DevAndroidAaron <aaron@okboy.app>
2021-11-29 09:42:13 -0600

    AppDatabase

---
+++ app/src/main/java/com/example/themovietest/data/local/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.themovietest.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.themovietest.data.model.MovieEntity
+
+@Database(entities = [MovieEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            INSTANCE = INSTANCE ?: Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "movie_table"
+            ).build()
+            return INSTANCE!!
+        }
+
+        fun destroyInstance(){
+            INSTANCE = null
+        }
+    }
+}
